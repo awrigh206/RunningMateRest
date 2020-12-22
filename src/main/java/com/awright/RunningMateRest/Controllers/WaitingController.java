36c3f6eec8d2596f7efb2c115f50b2b0c1d6f8e3
awrigh206 <awrigh206@caledonian.ac.uk>
2020-12-22 15:37:15 +0000

    bug fixes

--- src/main/java/com/awright/RunningMateRest/Controllers/WaitingController.java
+++ src/main/java/com/awright/RunningMateRest/Controllers/WaitingController.java
@@ -1,12 +1,17 @@
 package com.awright.RunningMateRest.Controllers;
 
 import java.util.List;
-
 import com.awright.RunningMateRest.DTO.UserDto;
 import com.awright.RunningMateRest.Models.User;
 import com.awright.RunningMateRest.Repositories.WaitingRepo;
 import com.awright.RunningMateRest.Services.WaitingService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WaitingController {
     @Autowired
     private WaitingService waitingService;
+    private final Log log = LogFactory.getLog(WaitingController.class);
 
     @PostMapping
     public void addToWaiting(@RequestBody UserDto userDto){
@@ -31,8 +37,15 @@ public class WaitingController {
     }
 
     @GetMapping
-    public List<String> getWaitingList(){
-        return waitingService.getWaitingList();
+    public ArrayNode getWaitingList(){
+        log.info("waiting: " + waitingService.getWaitingList());
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode node = mapper.createArrayNode();
+        
+        for(String current : waitingService.getWaitingList()){
+            node.add(current);
+        }
+        return node;
     }
 
     @DeleteMapping
